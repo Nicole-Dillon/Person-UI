3824937cd519624df611d95227a90d00e6eddc46
Nicole Dillon <nkdvgd@mail.missouri.edu>
2019-02-12 23:37:35 -0600

    initial commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Nicole Dillon on 2/12/19.
+//  Copyright © 2019 Nicole Dillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
